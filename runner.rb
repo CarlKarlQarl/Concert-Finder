fc24d6cc5490665475b24c989bc8e7da9ddcc63a
Karl Matthes <karlmatthes@gmail.com>
2019-12-05 09:08:32 -0700

    Got find_city function and printing out formatted results

--- runner.rb
+++ runner.rb
@@ -1,9 +1,4 @@
 require_relative 'config/environment.rb'
+ActiveRecord::Base.logger = nil
 
-Artist.load(JSON.parse(File.read('./db/artist.json')))
-Venue.load(JSON.parse(File.read('./db/venues.json')))
-Event.load(JSON.parse(File.read('./db/events.json')))
-
-Artist.by_id(310565)
-binding.pry
 Menu.new.start_menu
