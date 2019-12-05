2386f4c106b46262acdf67a9964f216da3009549
Karl Matthes <karlmatthes@gmail.com>
2019-12-05 12:27:18 -0700

    Some clean-up of old comments

--- lib/menu.rb
+++ lib/menu.rb
@@ -7,9 +7,8 @@ class Menu
     end
 
     def main_menu
-        loop_menu = true
         system "clear"
-        while loop_menu do
+        while true do
 
         puts "--How would you like to search for an event?--".white.on_black
         puts "1) Search by city"
@@ -21,13 +20,10 @@ class Menu
         user_input = gets.chomp
             case user_input
             when "1"
-                #system "clear"
                 city_search_menu
             when "2"
-                #system "clear"
                 artist_search_menu
             when "3"
-                #system "clear"
                 venue_search_menu
             when "0"
                 puts "Thank you for using Concert Finder!"
@@ -36,7 +32,6 @@ class Menu
                 puts ""
                 puts "Please retry with a valid menu option.".red
                 puts ""
-                #main_menu
             end
         end
     end
@@ -44,36 +39,36 @@ class Menu
     def city_search_menu
         system "clear"
         puts "--City Search--".white.on_black
-        print "Enter your city's zip code (q to quit): "
-        user_city = gets.chomp
-        return_to_main_menu user_city
-        puts ""
+        while true do
+            print "Enter your city's zip code (q to quit): "
+            user_city = gets.chomp
+            return_to_main_menu user_city
+            puts ""
 
-
-        if Venue.find_cities(user_city).length == 0
-            puts "No results found for that zip code."
-        else
             venues = Venue.find_cities(user_city)
 
-            relevant_events = []
+            if venues.length == 0
+                puts "No results found for that zip code."
+            else
+                relevant_events = []
 
-            venues.each do |venue|
-                relevant_events << venue.get_events
+                venues.each do |venue|
+                    relevant_events << venue.get_events
+                end
+            
+                relevant_events.flatten!
+
+                puts "Here are the events for #{user_city}:"
+                relevant_events.length.times do |index|
+                    puts "#{index}) #{relevant_events[index].artist.artistname} played at #{relevant_events[index].venue.name} on #{relevant_events[index].playdate}"
+                end
+
+                puts ""
+                print "Press Return to go back to the main menu..."
+
+                pause = gets
+                start_menu
             end
-        
-            relevant_events.flatten!
-
-            puts "Here are the events for #{user_city}:"
-            relevant_events.length.times do |index|
-                puts "#{index}) #{relevant_events[index].artist.artistname} played at #{relevant_events[index].venue.name} on #{relevant_events[index].playdate}"
-            end
-
-            puts ""
-            print "Press Return to go back to the main menu..."
-            pause = gets
-            #For testing, looping back to beginning
-            #system "clear"
-            start_menu
         end
     end
 
@@ -100,17 +95,14 @@ class Menu
                 puts "#{result.artistname} played #{shows.length} show(s)..."
                 shows.each {|event| puts "#{event.venue.name} on #{event.playdate}" }
 
-
                 puts ""
                 print "Press Return to go back to the main menu..."
+
                 pause = gets
-                #For testing, looping back to beginning
-                system "clear"
                 start_menu
             else
                 puts "No results found for '#{user_artist}'. Try again."
                 puts ""
-                #artist_search_menu
             end
         end
     end
@@ -131,14 +123,12 @@ class Menu
             #binding.pry
             puts "Here are the upcoming events at #{user_venue}:"
             puts ""
-            #Run find_venue again and post results here
-            #Hard coded sample below
             events.each {|event| puts "#{event.artist.artistname} played on #{event.playdate}"}
 
             puts ""
             print "Press Return to go back to the main menu..."
+
             pause = gets
-            #For testing, looping back to beginning
             start_menu
         else
             puts "No results found for that venue. Try again."
@@ -153,21 +143,5 @@ class Menu
             system "clear"
             start_menu
         end
-    end
-
-    #Placeholders for searching ActiveRecord
-    #Currently returning true and false
-    #Might be better if successful returns were checked base on
-    #the length of the search result
-    #i.e. length = 0 means no results, length > 0 means success
-
-    def find_venue venue
-        #Receives the venue name that the user entered
-        #Returns events at that venue
-        #If no results are found, returns nil
-        #For now, will return true or false, as needed for testing
-
-        true
-        #false
     end
 end
